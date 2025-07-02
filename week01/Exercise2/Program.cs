00db02781ae0f529fe985db253de8b788ce64137
angelvictor2 <avictorescobedo@byupathway.edu>
2025-07-02 16:30:22 -0600

    excersise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Please introduce your grade percentage: ");
+        string user_input = Console.ReadLine();
+        int grade = int.Parse(user_input);
+
+        string bowel = "";
+
+        if (grade > 90)
+        {
+            bowel = "A";
+        }
+
+        else if (grade > 80)
+        {
+            bowel = "B";
+        }
+
+        else if (grade > 70)
+        {
+            bowel = "C";
+        }
+
+        else if (grade > 60)
+        {
+            bowel = "D";
+        }
+
+        else
+        {
+            bowel = "F";
+        }
+
+        Console.WriteLine($"Your grade score is: {bowel}");
+
+        if (grade > 60)
+        {
+            Console.WriteLine("Congratulations you passed");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry, You failed this course");
+        }
+
     }
 }
