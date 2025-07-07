4a1f1e2bccccfdee58707b03ca6f003ac891d9b9
angelvictor2 <avictorescobedo@byupathway.edu>
2025-07-07 10:37:45 -0600

    week02 learning activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job = new Job();
+        job._jobTitle = "Sofware Engineer";
+        job._company = "Apple";
+        job._startYear = 2010;
+        job._endYear = 2015;
+        
+
+        Job job1 = new Job();
+        job1._jobTitle = "Business Manager";
+        job1._company = "Google";
+        job1._startYear = 2015;
+        job1._endYear = 2018;
+        
+
+        Resume resume = new Resume();
+        resume._name = "Ana";
+        resume._jobs.Add(job);
+        resume._jobs.Add(job1);
+
+        resume.ResumeDisplay();
+        
     }
 }
